e5e053c73fa6ea86af86226155f988ee86f37d4a
JerrySauer <susansmith801a461639842@outlook.com>
2024-03-22 04:26:00 +0800

    enim

---
+++ src/exception.ts
@@ -0,0 +1,87 @@
+import Status = require('http-status');
+
+interface ExceptionBaseProps<T = any> {
+    code?: number;
+    subcode?: number;
+    meta?: T;
+}
+
+interface ExceptionDefination<T> extends ExceptionBaseProps<T> {
+    message?: string;
+}
+
+export default class Exception<T extends string = 'Exception', U = any> extends Error {
+    readonly name: T;
+    readonly code: number;
+    readonly subcode: number;
+
+    readonly meta: U;
+
+    constructor(message: string = 'Unknown', meta?: U) {
+        super(message);
+        this.meta = meta;
+        this.code = 500;
+        this.subcode = 0;
+        this.name = 'Exception' as T;
+
+        Object.defineProperties(this, {
+            toJSON: {
+                value: Exception.prototype.toJSON,
+                enumerable: false,
+                writable: false,
+                configurable: false,
+            },
+        });
+    }
+
+    toJSON() {
+        return {
+            code: this.code,
+            subcode: this.subcode,
+            message: `${this.name}: ${this.message}`,
+            meta: this.meta,
+        };
+    }
+}
+
+export const define = <T, U = any>(name: T, define: ExceptionDefination<U> = {}): typeof Exception => {
+    const {
+        code = 500,
+        subcode = 0,
+        message: defaultMessage = (Status as any)[code],
+        meta: defaultMeta,
+    } = define;
+
+    function CustomException(message: string = defaultMessage, meta: U = defaultMeta): Exception {
+        const exception = new Exception(message, meta);
+
+        Object.defineProperties(exception, {
+            code: {
+                get() {
+                    return code;
+                },
+                configurable: false,
+            },
+            subcode: {
+                get() {
+                    return subcode;
+                },
+                configurable: false,
+            },
+            name: {
+                get() {
+                    return name;
+                },
+                configurable: false,
+            },
+        });
+
+        return exception;
+    }
+
+    // for nodejs stack
+    CustomException.captureStackTrace = Exception.captureStackTrace;
+    CustomException.stackTraceLimit = Exception.stackTraceLimit;
+
+    return CustomException as any;
+};
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Exception from './exception';
+
+export * from './exception';
+export * from './http-errors';
+
+export default Exception;
